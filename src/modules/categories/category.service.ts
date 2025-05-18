52cf70d8b2b03f673a8227496982faccb7320312
Md Shohel Rana <shohel.xponent@gmail.com>
2025-05-18 12:55:54 +0600

    refactor: category service

--- src/modules/categories/category.service.ts
+++ src/modules/categories/category.service.ts
@@ -12,26 +12,38 @@ import { CreateCategoryDto, UpdateCategoryDto } from "./category.dto";
 export const createCategory = async (
   data: CreateCategoryDto
 ): Promise<Category> => {
-  return await prisma.category.create({
-    data,
-  });
+  try {
+    return await prisma.category.create({
+      data,
+    });
+  } catch (error) {
+    throw new Error(`Error creating category: ${getErrorMessage(error)}`);
+  }
 };
 
 // Get all categories
 export const getAllCategories = async (): Promise<Category[]> => {
-  return await prisma.category.findMany();
+  try {
+    return await prisma.category.findMany();
+  } catch (error) {
+    throw new Error(`Error fetching categories: ${getErrorMessage(error)}`);
+  }
 };
 
 // Get a category by ID
 export const getCategoryById = async (
   categoryId: BigInt
 ): Promise<Category | null> => {
-  return await prisma.category.findUnique({
-    where: { categoryId: Number(categoryId) },
-    // include: {
-    //   products: true, // Include related products if needed
-    // },
-  });
+  try {
+    return await prisma.category.findUnique({
+      where: { categoryId: Number(categoryId) },
+      include: {
+        products: true, // Include related products if needed
+      },
+    });
+  } catch (error) {
+    throw new Error(`Error fetching category by ID: ${getErrorMessage(error)}`);
+  }
 };
 
 // Update a category's details
@@ -39,15 +51,23 @@ export const updateCategory = async (
   categoryId: bigint,
   data: UpdateCategoryDto
 ): Promise<Category> => {
-  return await prisma.category.update({
-    where: { categoryId: Number(categoryId) },
-    data,
-  });
+  try {
+    return await prisma.category.update({
+      where: { categoryId: Number(categoryId) },
+      data,
+    });
+  } catch (error) {
+    throw new Error(`Error updating category: ${getErrorMessage(error)}`);
+  }
 };
 
 // Delete a category
 export const deleteCategory = async (categoryId: BigInt): Promise<Category> => {
-  return await prisma.category.delete({
-    where: { categoryId: Number(categoryId) },
-  });
+  try {
+    return await prisma.category.delete({
+      where: { categoryId: Number(categoryId) },
+    });
+  } catch (error) {
+    throw new Error(`Error deleting category: ${getErrorMessage(error)}`);
+  }
 };
