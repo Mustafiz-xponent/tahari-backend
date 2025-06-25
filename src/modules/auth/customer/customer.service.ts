1baec0c5e60926a964f029c9f174c5e0d447712f
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-06-25 10:29:50 +0600

    chore:verify customer service

--- src/modules/auth/customer/customer.service.ts
+++ src/modules/auth/customer/customer.service.ts
@@ -162,8 +162,9 @@ export async function verifyCustomerOtp(
     data: { status: "ACTIVE" },
     include: {
       customer: {
-        include: {
-          wallet: true,
+        select: {
+          customerId: true,
+          wallet: { select: { walletId: true } },
         },
       },
     },
