7252f95202c93c1d4c5a1194510ab50b6dde17cd
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-07-08 17:30:08 +0600

    fix: issues

--- src/prisma-client/prismaClient.ts
+++ src/prisma-client/prismaClient.ts
@@ -2,16 +2,6 @@ import { PrismaClient } from "@/generated/prisma/client";
 
 const isTest = process.env.NODE_ENV === "test";
 
-// const prisma = new PrismaClient({
-//   datasources: {
-//     db: {
-//       url: isTest
-//         ? "file:./test.db?mode=memory&cache=shared"
-//         : process.env.DATABASE_URL,
-//     },
-//   },
-// });
-
 const prisma = new PrismaClient({
   datasources: {
     db: {
@@ -29,24 +19,3 @@ const prisma = new PrismaClient({
 });
 
 export default prisma;
-// import { PrismaClient } from "@/generated/prisma/client";
-
-// const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-// const isTest = process.env.NODE_ENV === "test";
-
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     datasources: {
-//       db: {
-//         url: isTest
-//           ? "file:./test.db?mode=memory&cache=shared"
-//           : process.env.DATABASE_URL,
-//       },
-//     },
-//   });
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// export default prisma;
