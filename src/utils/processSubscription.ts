b57531c9f4376f639d051ae38e58052c0c936467
Omar-dev-xponent <omar12.xponent@gmail.com>
2025-08-31 09:19:51 +0600

    feat: emit support/admin subscription order created

--- src/utils/processSubscription.ts
+++ src/utils/processSubscription.ts
@@ -17,6 +17,8 @@ import logger from "@/utils/logger";
 import { Prisma } from "@prisma/client";
 import prisma from "@/prisma-client/prismaClient";
 import { createNotification } from "@/utils/processPayment";
+import { getOnlineSupportSockets, io } from "@/utils/socket";
+import * as orderService from "@/modules/orders/orders.service";
 
 // Types
 export type SubscriptionWithRelations = {
@@ -249,6 +251,15 @@ export const createOrderWithItems = async (
       },
     ],
   });
+
+  // Emit Support/Admin
+  if (order) {
+    const orderData = await orderService.getOrderById(order.orderId);
+    const sockets = getOnlineSupportSockets();
+    sockets.forEach((socketId) => {
+      io.to(socketId).emit("newOrder", orderData);
+    });
+  }
   return order;
 };
 export const createSubscriptionDelivery = async (
